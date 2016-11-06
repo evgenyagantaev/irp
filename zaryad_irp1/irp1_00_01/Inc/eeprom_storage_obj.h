2ca3c478a67507550693a2369216a2c32b66cedf
Evgeny Agantaev <agantaev@mail.ru>
2016-11-06 17:58:54 +0300

    technical; nachal delat' zapis' vo flesh;

---
+++ zaryad_irp1/irp1_00_01/Inc/eeprom_storage_obj.h
@@ -0,0 +1,33 @@
+/*
+ * eeprom_storage_obj.h
+ *
+ *  Created on: Nov 6, 2016
+ *      Author: root
+ */
+
+#ifndef INC_EEPROM_STORAGE_OBJ_H_
+#define INC_EEPROM_STORAGE_OBJ_H_
+
+
+#define EEPROM_MARK 0x01234567
+#define EEPROM_BASE_ADDRESS 0x08080000
+#define EEPROM_MARK_ADDRESS 0x00000000 + 0x08080000
+#define EEPROM_DISCHCAP_ADDRESS 0x00000004 + 0x08080000
+#define EEPROM_REMCAP_ADDRESS 0x00000008 + 0x08080000
+
+// ***** EEPROM STORAGE MAP *****
+
+// base address 0x0808 0000
+// addr 0x0000 0000 priznak togo, chto v eeprom uzhe ran'she pisali kakie-to znacheniya (0x0123 4567)
+// addr 0x0000 0004 razryadnaya emkost' (zapisyvaetsya po okonchanii polnogo razryada)
+// addr 0x0000 0008 ostatochnaya emkost' (obnovlyaetsya raz v 10 minut)
+
+
+void eeprom_write_mark();
+void eeprom_write_discharge_capacity(uint32_t discharge_capaciy);
+uint32_t eeprom_read_discharge_capacity();
+void eeprom_write_remaining_capacity(uint32_t remaining_capaciy);
+uint32_t eeprom_read_remaining_capacity();
+
+
+#endif /* INC_EEPROM_STORAGE_OBJ_H_ */
---
+++ zaryad_irp1/irp1_00_01/Src/eeprom_storage_obj.c
@@ -0,0 +1,42 @@
+/*
+ * eeprom_storage_obj.c
+ *
+ *  Created on: Nov 6, 2016
+ *      Author: root
+ */
+
+#include "eeprom_storage_obj.h"
+#include "stm32l0xx_hal.h"
+
+
+void eeprom_write_mark()
+{
+	HAL_FLASHEx_DATAEEPROM_Unlock();
+	HAL_FLASHEx_DATAEEPROM_Erase(EEPROM_MARK_ADDRESS);
+	HAL_FLASHEx_DATAEEPROM_Program(FLASH_TYPEPROGRAMDATA_WORD, EEPROM_MARK_ADDRESS, EEPROM_MARK);
+	HAL_FLASHEx_DATAEEPROM_Lock();
+}
+
+
+void eeprom_write_discharge_capacity(uint32_t discharge_capaciy)
+{
+	HAL_FLASHEx_DATAEEPROM_Unlock();
+	HAL_FLASHEx_DATAEEPROM_Erase(EEPROM_DISCHCAP_ADDRESS);
+	HAL_FLASHEx_DATAEEPROM_Program(FLASH_TYPEPROGRAMDATA_WORD, EEPROM_DISCHCAP_ADDRESS, discharge_capaciy);
+	HAL_FLASHEx_DATAEEPROM_Lock();
+}
+uint32_t eeprom_read_discharge_capacity()
+{
+	return *(__IO uint32_t *)EEPROM_DISCHCAP_ADDRESS;
+}
+void eeprom_write_remaining_capacity(uint32_t remaining_capaciy)
+{
+	HAL_FLASHEx_DATAEEPROM_Unlock();
+	HAL_FLASHEx_DATAEEPROM_Erase(EEPROM_REMCAP_ADDRESS);
+	HAL_FLASHEx_DATAEEPROM_Program(FLASH_TYPEPROGRAMDATA_WORD, EEPROM_REMCAP_ADDRESS, remaining_capaciy);
+	HAL_FLASHEx_DATAEEPROM_Lock();
+}
+uint32_t eeprom_read_remaining_capacity()
+{
+	return *(__IO uint32_t *)EEPROM_REMCAP_ADDRESS;
+}
